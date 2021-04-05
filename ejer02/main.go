5779f367d305a006795e48ca5c2180b36051074b
Samuel Paez <samuel005005@gmail.com>
2021-04-05 15:54:15 -0400

    eje01

---
+++ ejer02/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var numero int
+var texto string
+var status bool
+
+// float32 float64
+// int8 int16 int32 int64
+// uint int sin signo
+
+func main() {
+	var numero1, numero2 int
+	numero2, numero3, texto := 3, 4, "Hola"
+	hola := "hola mundo"
+
+	fmt.Print(hola, numero1, numero2, numero3, texto)
+}
