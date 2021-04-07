2be11dd03c6358263131bd56c12bebb58e3005ce
Samuel Paez <samuel005005@gmail.com>
2021-04-07 09:12:36 -0400

    ejecicio 2

--- ejer02/main.go
+++ ejer02/main.go
@@ -4,16 +4,19 @@ import "fmt"
 
 var numero int
 var texto string
-var status bool
+var status bool = true
 
 // float32 float64
 // int8 int16 int32 int64
 // uint int sin signo
-
 func main() {
 	var numero1, numero2 int
 	numero2, numero3, texto := 3, 4, "Hola"
 	hola := "hola mundo"
 
-	fmt.Print(hola, numero1, numero2, numero3, texto)
+	fmt.Println(hola)
+	fmt.Println(numero1)
+	fmt.Println(numero2)
+	fmt.Println(numero3)
+	fmt.Println(texto)
 }
